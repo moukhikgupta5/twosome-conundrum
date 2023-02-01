6a54b762b762e78703976bd14f27b448f4903081
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2023-02-02 00:31:41 +0530

    leader email check

--- Controllers/signup.js
+++ Controllers/signup.js
@@ -4,14 +4,22 @@ const signuphandler = async (req, res) => {
   try {
     const input_Data = req.body;
     console.log(input_Data);
-  
+    const Leadercheck = await dataModel.findOne({
+      Leader_email: input_Data.Leader_email,
+    });
+    if (Leadercheck) {
+      res.json({
+        message: "Team leader or Team Memeber already signed up",
+        correct: false,
+      });
+    } else {
       await dataModel.create(input_Data);
       res.json({
         message: "Team Signed Up",
         data: input_Data,
         correct: true,
       });
-    
+    }
   } catch (err) {
     console.log(err);
     res.json({
