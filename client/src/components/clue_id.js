83af768a176936168b6ecdfceb4af87ef7ffd164
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2023-02-05 02:24:33 +0530

    clues added

--- client/src/components/clue_id.js
+++ client/src/components/clue_id.js
@@ -1,9 +1,184 @@
 const clue_data=[{
-    name:"Hello",
-    id:"384509"
+    name:"Autopsy(Kumkum)",
+    id:"63de93590dfcdbd22460e452"
 },
 {
-    name:"laddi",
-    id:"63d8cd3f89d3258f2b463d9b"
-}]
+    name:"Autopsy(Prachi)",
+    id:"63de93ce0dfcdbd22460e454"
+},
+{
+    name:"Call Records(Bhavuk)",
+    id:"63de94770dfcdbd22460e456"
+},
+{
+    name:"Call Records(Divyansh)",
+    id:"63de94a00dfcdbd22460e458"
+},
+{
+    name:"Call Records(Guarisha)",
+    id:"63de94c20dfcdbd22460e45a"
+},
+{
+    name:"Call Records(Kumkum)",
+    id:"63de94f80dfcdbd22460e45c"
+},
+{
+    name:"Call Records(Manager)",
+    id:"63de951b0dfcdbd22460e45e"
+},
+{
+    name:"Call Records(Prachi)",
+    id:"63de95350dfcdbd22460e460"
+},
+{
+    name:"Call Records(Sanhita)",
+    id:"63de95500dfcdbd22460e462"
+},
+{
+    name:"Call Records(Sanket)",
+    id:"63de956b0dfcdbd22460e464"
+},
+{
+    name:"Call Records(Servent)",
+    id:"63de95930dfcdbd22460e466"
+},
+{
+    name:"Call Records(Medios)",
+    id:"63de95a90dfcdbd22460e468"
+},{
+    name:"Car Report(Story 1)",
+    id:"63deaa99e3f8c96aa8625842"
+},
+{
+    name:"Character Sketch(Jatin)",
+    id:"63deab1ce3f8c96aa8625844"
+},
+{
+    name:"Character Sketch(Kumkum)",
+    id:"63deab90e3f8c96aa8625846"
+},
+{
+    name:"Character Sketch(Manager)",
+    id:"63deabd0e3f8c96aa8625848"
+},
+{
+    name:"Character Sketch(Nishant)",
+    id:"63deabede3f8c96aa862584a"
+},
+{
+    name:"Character Sketch(Sanket)",
+    id:"63deac06e3f8c96aa862584c"
+},
+{
+    name:"Character Sketch(Servent)",
+    id:"63deac1de3f8c96aa862584e"
+},
+{
+    name:"Evidences(CCTV)",
+    id:"63deb257fb76150c3e6fd834"
+},
+{
+    name:"Evidences(CD)",
+    id:"63deb274fb76150c3e6fd836"
+},
+{
+    name:"Evidences(Letter Report)",
+    id:"63deb291fb76150c3e6fd838"
+},{
+    name:"Evidences(Prachi Letter)",
+    id:"63deb2adfb76150c3e6fd83a"
+},
+{
+    name:"Evidences(Picture)",
+    id:"63deb2cffb76150c3e6fd83c"
+},
+{
+    name:"Investigation Report(Kumkum e1)",
+    id:"63deb401fb76150c3e6fd83e"
+},
+{
+    name:"Investigation Report(Kumkum e2)",
+    id:"63deb41cfb76150c3e6fd840"
+},
+{
+    name:"Investigation Report(Prachi e1)",
+    id:"63deb432fb76150c3e6fd842"
+},
+{
+    name:"Investigation Report(Prachi e2)",
+    id:"63deb448fb76150c3e6fd844"
+},
+{
+    name:"Investigation Report(Prachi e3)",
+    id:"63deb45efb76150c3e6fd846"
+},
+{
+    name:"Investigation Report(Prachi e4)",
+    id:"63deb476fb76150c3e6fd848"
+},
+{
+    name:"Prespection",
+    id:"63deb4a2fb76150c3e6fd84a"
+},
+{
+    name:"Statement(Bhavuk)",
+    id:"63deb611fb76150c3e6fd84c"
+},
+{
+    name:"Statement(Divyansh 1)",
+    id:"63deb63afb76150c3e6fd84e"
+},
+{
+    name:"Statement(Divyansh 2)",
+    id:"63deb64efb76150c3e6fd850"
+},
+{
+    name:"Statement(Gaurisha 1)",
+    id:"63deb66efb76150c3e6fd852"
+},
+{
+    name:"Statement(Gaurisha 2)",
+    id:"63deb680fb76150c3e6fd854"
+},
+{
+    name:"Statement(Kumkum 1)",
+    id:"63deb69dfb76150c3e6fd856"
+},
+{
+    name:"Statement(Kumkum 2)",
+    id:"63deb6b3fb76150c3e6fd858"
+},
+{
+    name:"Statement(Manager)",
+    id:"63deb6cafb76150c3e6fd85a"
+},
+{
+    name:"Statement(Medical)",
+    id:"63deb6e2fb76150c3e6fd85c"
+},
+{
+    name:"Statement(Sanhita)",
+    id:"63deb6f8fb76150c3e6fd85e"
+},
+{
+    name:"Statement(Sanket)",
+    id:"63deb72bfb76150c3e6fd860"
+},
+{
+    name:"Statement(Servent 1)",
+    id:"63deb746fb76150c3e6fd862"
+},
+{
+    name:"Statement(Servent 2)",
+    id:"63deb757fb76150c3e6fd864"
+},
+{
+    name:"Statement(Service Centre)",
+    id:"63deb76efb76150c3e6fd866"
+},
+{
+    name:"Crime Report",
+    id:"63deb78afb76150c3e6fd868"
+}
+]
 module.exports=clue_data;
