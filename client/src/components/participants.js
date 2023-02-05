085e923c380886e6ea72f0da42b8a5825b11990b
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2023-02-05 11:58:20 +0530

    mails

--- client/src/components/participants.js
+++ client/src/components/participants.js
@@ -1,9 +1,184 @@
 const participant_data=[{
     email:"test123@gmail.com",
+    no:"0"
+},
+{
+    email:"lakshay13062004@gmail.com",
     no:"1"
 },
 {
-    email:"test@gmail.com",
+    email:"rohillagogo123@gmail.com",
     no:"2"
-}]
+},
+{
+    email:"ssharma_be20@thapar.edu",
+    no:"3"
+},
+{
+    email:"mchhouda_be21@thapar.edu",
+    no:"4"
+},
+{
+    email:"akmarkan28@gmail.com",
+    no:"5"
+},
+{
+    email:"rtoteja_be22@thapar.edu",
+    no:"6"
+},
+{
+    email:"snarang_be22@thapar.edu",
+    no:"7"
+},
+{
+    email:"psingla1_be21@thapar.edu",
+    no:"8"
+},
+{
+    email:"rabhijeetsingh_27@gmail.com",
+    no:"9"
+},
+{
+    email:"gunsheen19@gmail.com",
+    no:"10"
+},
+{
+    email:"astikthukral25@gmail.com",
+    no:"11"
+},
+{
+    email:"twinkle05gawri@gmail.com",
+    no:"12"
+},
+{
+    email:"dsharma1_be22@thapar.edu",
+    no:"13"
+},
+{
+    email:"archimatta6@gmail.com",
+    no:"14"
+},
+{
+    email:"ksingh10_be22@thapar.edu",
+    no:"15"
+},
+{
+    email:"knewmei_mtech22@thapar.edu",
+    no:"16"
+},
+{
+    email:"ykumar_be22@thapar.edu",
+    no:"17"
+},
+{
+    email:"ssharma_be22@thapar.edu",
+    no:"18"
+},
+{
+    email:"ssharma_be21@thapar.edu",
+    no:"19"
+},
+{
+    email:"nsingla1_be22@thapar.edu",
+    no:"20"
+},
+{
+    email:"raggarwal_be22@thapar.edu",
+    no:"21"
+},
+{
+    email:"sagnohotri_be22@thapar.edu",
+    no:"22"
+},
+{
+    email:"agupta27_be22@thapar.edu",
+    no:"23"
+},
+{
+    email:"asrivastava1_be20@thapar.edu",
+    no:"24"
+},
+{
+    email:"aaastha1_be22@thapar.edu",
+    no:"25"
+},
+{
+    email:"tejaswaghe011@gmail.com",
+    no:"26"
+},
+{
+    email:"abatra1_be21@thapar.edu",
+    no:"27"
+},
+{
+    email:"ddishadhanda@gmail.com",
+    no:"28"
+},
+{
+    email:"sgummadapu_be22@thapar.edu",
+    no:"29"
+},
+{
+    email:"lavanyajain82605@gmail.com",
+    no:"30"
+},
+{
+    email:"pparnita_be22@thapar.edu",
+    no:"31"
+},
+{
+    email:"niyatiguptaza@gmail.com",
+    no:"32"
+},
+{
+    email:"piyushmehtasirsa@gmail.com",
+    no:"33"
+},
+{
+    email:"ekaur_be@thapar.edu",
+    no:"34"
+},
+{
+    email:"ahuraneakshat33@gmail.com",
+    no:"35"
+},
+{
+    email:"mdtyagi21jan@gmail.com",
+    no:"36"
+},
+{
+    email:"kgupta11_be21@thapar.edu",
+    no:"37"
+},
+{
+    email:"anantsinghchauhan1@gmail.com",
+    no:"38"
+},
+{
+    email:"mchaudhary1_be21@thapar.edu",
+    no:"39"
+},
+{
+    email:"mchaudhary1_be21@thapar.edu",
+    no:"40"
+},
+{
+    email:"akshatagrawal247@gmail.com",
+    no:"41"
+},
+{
+    email:"meharkapoor27@gmail.com",
+    no:"42"
+},
+{
+    email:"hchadha_be22@thapar.edu",
+    no:"43"
+},
+{
+    email:"rkhanna2_be20@thapar.edu",
+    no:"43"
+},
+
+
+]
 module.exports=participant_data;
